4845aaf77088204f076ac33801815cd9b312f0e8
JoaoGabrielTN <joaogt008@gmail.com>
2024-04-09 09:24:48 -0300

    consulta ao acervo added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const bem_vindo = document.getElementById('bem_vindo')
 const nossa_estrutura = document.getElementById('nossa_estrutura')
+const consulta_acervo = document.getElementById('consulta_acervo')
 const login_bt = document.getElementById('login')
 
 function goToPage(page){
@@ -12,6 +13,10 @@ bem_vindo.onclick = () => {
 
 nossa_estrutura.onclick = () => {
     goToPage('src/html/nossa_estrutura.html')
+}
+
+consulta_acervo.onclick = () => {
+    goToPage('src/html/consulta_acervo.html')
 }
 
 login_bt.onclick = () => {
