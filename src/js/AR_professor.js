df4f0e8ff41a0c330e6509a4e00920b36df39b91
flavio <flavio.00000844530@unicap.br>
2024-04-23 08:56:22 -0300

    a

--- src/js/AR_professor.js
+++ src/js/AR_professor.js
@@ -1,5 +1,6 @@
 const black = 'rgb(40, 37, 37)';
 
+const iframe = document.getElementById('iframe')
 const list_items = document.getElementById('prof').getElementsByTagName('li');
 
 let select = list_items[0]
@@ -27,4 +28,18 @@ for(let i = 0; i < list_items.length; i++)
             select = list_items[i];
         }
     })
+}
+
+const quadro = document.getElementById('quadro')
+const notas = document.getElementById('notas')
+const emprestimo = document.getElementById('_emprestimos')
+
+quadro.onclick = () => {
+    iframe.src = '../AR/Quadro2.html'
+}
+notas.onclick = () => {
+    iframe.src = '../AR/Notas2.html'
+}
+emprestimo.onclick = () => {
+    iframe.src = '../AR/emprestimo.html'
 }
